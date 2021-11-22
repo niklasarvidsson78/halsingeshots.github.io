d78bb251cb615404b632b3544226c606bb2789cc
niklasarvidsson78 <33256419+niklasarvidsson78@users.noreply.github.com>
2021-11-22 23:29:50 +0100

    211122_3

--- mail.php
+++ mail.php
@@ -6,11 +6,10 @@ $message= $_POST['message'];
 $to = "niklasarvidsson78@hotmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+$headers = "From: kontakt@halsingeshots.se";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:thankyou.html");
+//header("Location:thankyou.html");
 ?>
